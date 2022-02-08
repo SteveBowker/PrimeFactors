f2714ad72c222412b5ec15964d7fa5ec78bec576
Steve Bowker <sjbowker@btinternet.com>
2022-02-08 16:55:06 +0000

    PrimeFactors

---
+++ PrimeFactors/Form1.cs
@@ -0,0 +1,41 @@
+﻿using PrimeFactors.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeFactors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            IPrimeFactors obj = new PrimeFactorsClass();
+            long num = Convert.ToInt64(textBox1.Text);
+            List<long> nums = new List<long>();
+
+            nums = obj.PrimeFactors(num);
+
+            foreach (long number in nums)
+            {
+                richTextBox1.AppendText(number.ToString() + "\r\n");
+            }
+        }
+    }
+}
---
+++ PrimeFactors/Interfaces/IPrimeFactors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeFactors.Interfaces
+{
+public interface IPrimeFactors
+    {
+        List<long> PrimeFactors(long num);
+    }
+
+    class PrimeFactorsClass : IPrimeFactors
+    {
+        List<long> IPrimeFactors.PrimeFactors(long num)
+        {
+            // Return the number's prime factors.
+            {
+                List<long> result = new List<long>();
+
+                // Take out the 2s.
+                while (num % 2 == 0)
+                {
+                    result.Add(2);
+                    num /= 2;
+                }
+
+                // Take out other primes.
+                long factor = 3;
+                while (factor * factor <= num)
+                {
+                    if (num % factor == 0)
+                    {
+                        // This is a factor.
+                        result.Add(factor);
+                        num /= factor;
+                    }
+                    else
+                    {
+                        // Go to the next odd number.
+                        factor += 2;
+                    }
+                }
+
+                // If num is not 1, then whatever is left is prime.
+                if (num > 1) result.Add(num);
+
+                return result;
+            }
+        }
+
+    }
+}
